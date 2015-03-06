785a46fe40e0fe39b98accc0476142d5278b63fe
kevintee <kevintee@berkeley.edu>
2015-03-06 11:20:56 -0800

    Experiments file for trying out new algorithms

---
+++ src/experiments.py
@@ -0,0 +1,12 @@
+# Add your experiments here
+
+from parse import parse_data
+
+def sample_experiment(X):
+    pass
+
+def main():
+    X, regulators = parse_data()
+
+if __name__ == '__main__':
+    main()
