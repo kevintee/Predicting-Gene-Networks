7fb214ce61af477a51f24d90003d4cb026fb7ef6
kevintee <kevintee@berkeley.edu>
2015-04-06 15:42:45 -0700

    run_merlin script

---
+++ run_merlin.sh
@@ -0,0 +1,11 @@
+# Runs MERLIN and places output in folder results/
+# There is a seg fault, but some results are inputted into results.
+
+cd gpdream/modules/Merlin/gsl-1.9;
+./configure;
+make;
+sudo make install;
+cd ../src;
+make;
+mkdir results;
+./merlin -d example/net1_expression.txt -l example/net1_transcription_factors.tsv -o results/;
