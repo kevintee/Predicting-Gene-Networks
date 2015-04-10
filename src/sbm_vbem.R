a4df35be4db9ac8d917823f7f35efaa58e96c111
kevintee <kevintee@berkeley.edu>
2015-04-09 22:05:42 -0700

    Create binary matrix using simple correlation

--- src/sbm_vbem.R
+++ src/sbm_vbem.R
@@ -5,6 +5,19 @@
 setwd("/Users/kevintee/Downloads/Predicting-Gene-Networks/src/")
 
 # Read in files
-fileName <- "data/BRCA.txt"
-allData <- read.table(fileName, header=T, sep="\t")
-allData <- allData[,-1]
+# X is in the form: Genes vs Tumor Sample
+fileName <- "data/KIRC.txt"
+rawData <- read.table(fileName, header=T, sep="\t")
+rawData <- rawData[,-1] # Remove first column
+
+# Define constants
+EPSILON <- 1e-6 # Threshold for termination
+Q <- 10 # Number of classes
+N <- 8499 # Number of genes
+LAMBDA <- 0.5 # Sparsity for binary matrix
+
+# Generate binary matrix via correlation cutoff
+# TODO(kevintee): better way of generating binary matrix
+X = cor(t(rawData), t(rawData))
+X[abs(X) < LAMBDA] = 0 # Remove uncorrelated values
+X[X != 0] = 1 # Set all nonzero values to 1 for binary matrix
