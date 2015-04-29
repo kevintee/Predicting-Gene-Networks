de07f29cbb13f5cecffe3db185e0e6fdbf621a8a
kevintee <kevintee@berkeley.edu>
2015-04-29 15:55:56 -0700

    Same universe for comparison

--- src/metrics.py
+++ src/metrics.py
@@ -1,9 +1,8 @@
 # Add your metrics here
 
 import scipy.stats
+from itertools import chain
 from parse import parse_sbm_results, parse_chip_seq
-
-key_tf = 'TFAP2A'
 
 def score_gene_weights(test_vals, true_vals):
     intersect = set(test_vals) & set(true_vals)
@@ -11,11 +10,33 @@ def score_gene_weights(test_vals, true_vals):
     true_unique = set(true_vals) - intersect
     #return scipy.stats.hypergeom.cdf
 
+# This method makes the 'universe' of both the SBM and ChIP-seq results the same
+def remove_unique_genes(sbm_results, chip_results):
+    # First get the intersection of the two sets
+    overlapping_genes = set(chain(*sbm_results.values()))
+    overlapping_genes &= set(chain(*chip_results.values()))
+
+    # Remove all results that aren't in either of the two sets
+    sbm_modified = {}
+    chip_modified = {}
+    for k,v in sbm_results.items():
+        if k in overlapping_genes:
+            v = list(set(v) & overlapping_genes)
+            sbm_modified[k] = v
+
+    for k,v in chip_results.items():
+        if k in overlapping_genes:
+            v = list(set(v) & overlapping_genes)
+            chip_modified[k] = v
+
+    return sbm_modified, chip_modified
+
 def evaluate_network():
     tfs, genes, sbm_results, sbm_module_to_gene, \
         sbm_gene_to_module = parse_sbm_results()
 
     chip_results = parse_chip_seq()
+    sbm_results, chip_results = remove_unique_genes(sbm_results, chip_results)
     return score_gene_weights(sbm_results, chip_results)
 
 def main():
